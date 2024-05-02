6fe00c7e15df5ec4e2e8be70db864925b4c3ab03
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-02 14:35:50 +0900

    parserファイルをリファクタリング

--- pkg/mysqllog/parser.go
+++ pkg/mysqllog/parser.go
@@ -9,6 +9,7 @@ import (
 	"github.com/percona/go-mysql/query"
 )
 
+// Query 構造体の定義は変更なし
 type Query struct {
 	Time         string
 	User         string
@@ -23,53 +24,66 @@ type Query struct {
 	Fingerprint  string
 }
 
-func Parser(r io.Reader) Query {
-	scanner := bufio.NewScanner(r)
-	var queries []string
-	var q Query
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.Split(line, " ")
-		if parts[0] != "#" {
-			queries = append(queries, line)
-		} else {
-			for i, part := range parts {
-				part = strings.ToLower(part)
-				if strings.Contains(part, "query_time:") {
-					q.QueryTime = parts[i+1]
-				} else if strings.Contains(part, "lock_time:") {
-					q.LockTime = parts[i+1]
-				} else if strings.Contains(part, "time:") {
-					q.Time = parts[i+1]
-				} else if strings.Contains(part, "user@host:") {
-					items := regexp.MustCompile(`\[(.*?)\]`).FindAllString(line, -1)
-					q.User = items[0]
-					q.Host = items[1]
-				} else if strings.Contains(part, "id:") {
-					q.ID = parts[i+1]
-				} else if strings.Contains(part, "rows_sent:") {
-					q.RowsSent = parts[i+1]
-				} else if strings.Contains(part, "rows_examined:") {
-					q.RowsExamined = parts[i+1]
-				} else if strings.Contains(part, "rows_affected:") {
-					q.RowsAffected = parts[i+1]
-				}
+// メタデータの解析を別関数に
+func parseMetadata(line string, q *Query) {
+	parts := strings.Fields(line)
+	for i, part := range parts {
+		part = strings.ToLower(part)
+		switch {
+		case strings.Contains(part, "query_time:"):
+			q.QueryTime = parts[i+1]
+		case strings.Contains(part, "lock_time:"):
+			q.LockTime = parts[i+1]
+		case strings.Contains(part, "time:"):
+			q.Time = parts[i+1]
+		case strings.Contains(part, "user@host:"):
+			items := regexp.MustCompile(`\[(.*?)\]`).FindAllString(line, -1)
+			if len(items) >= 2 {
+				q.User = items[0]
+				q.Host = items[1]
 			}
+		case strings.Contains(part, "id:"):
+			q.ID = parts[i+1]
+		case strings.Contains(part, "rows_sent:"):
+			q.RowsSent = parts[i+1]
+		case strings.Contains(part, "rows_examined:"):
+			q.RowsExamined = parts[i+1]
+		case strings.Contains(part, "rows_affected:"):
+			q.RowsAffected = parts[i+1]
 		}
 	}
-	q.Query = strings.Join(queries, " ")
+}
 
-	// NOTE: クエリを Fingerprint でマスクする
+// クエリの正規化を別関数に
+func normalizeQuery(queryText string) string {
 	var fingerprints []string
-	t := strings.Split(q.Query, ";")
-	for _, qq := range t {
-		// use <database> の <database> をマスクしてしまうので除外
-		// SET timestamp=<timestamp> の <timestamp> をマスクしてしまうので除外
+	queries := strings.Split(queryText, ";")
+	for _, qq := range queries {
 		if !strings.HasPrefix(qq, "use ") && !strings.HasPrefix(qq, "SET") {
 			qq = query.Fingerprint(qq)
 		}
 		fingerprints = append(fingerprints, qq)
 	}
-	q.Fingerprint = strings.Join(fingerprints, ";\n")
+	return strings.Join(fingerprints, ";\n")
+}
+
+// Parser 関数のリファクタリング
+func Parser(r io.Reader) Query {
+	scanner := bufio.NewScanner(r)
+	var queryText strings.Builder
+	var q Query
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			parseMetadata(line, &q)
+		} else {
+			queryText.WriteString(line + " ")
+		}
+	}
+
+	q.Query = queryText.String()
+	q.Fingerprint = normalizeQuery(q.Query)
+
 	return q
 }
